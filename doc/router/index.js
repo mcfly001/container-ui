76b829dc6efe81f6f85cb70f7a686ea1b9844e99
douchi <douchi@2dfire.com>
2018-09-05 19:24:44 +0800

    9.5

--- doc/router/index.js
+++ doc/router/index.js
@@ -1,7 +1,6 @@
 import Introduce from '../views/content/Introduce.md'
 import Develop from '../views/content/Develop.md'
 import OpenApp from '../views/content/OpenApp.md'
-import Test from '../views/content/Test.md'
 
 export const routes = [
   {
@@ -22,10 +21,5 @@ export const routes = [
     path: '/OpenApp',
     name: 'OpenApp',
     component: OpenApp
-  },
-  {
-    path: '/Test',
-    name: 'Test',
-    component: Test
   }
 ]
--- packages/Test/.postcssrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "plugins": {
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {
-      browsers: ['Android >= 4.0', 'iOS >= 7']
-    }
-  }
-}
