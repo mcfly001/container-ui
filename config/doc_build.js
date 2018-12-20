593192742f2dd66bdf98b4dd8331317c92199f0d
douchi <douchi@2dfire.com>
2018-12-20 17:10:35 +0800

    dd

--- config/doc_build.js
+++ config/doc_build.js
@@ -1,4 +1,4 @@
 export default {
   IFRAME_URL: 'http://wuliaoyuan.top:6767/container-ui/demo/index.html#',
-  JUMP_URL: 'http://wuliaoyuan.top:6766/component-ui/demo/index.html#'
+  JUMP_URL: 'http://wuliaoyuan.top:6766/component-ui/doc/index.html#'
 }
